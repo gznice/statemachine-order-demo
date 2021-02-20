689b6646423f67a2fc8a599e6f68aa37926db914
liuzhimin <vvnn@qq.com>
2021-02-20 18:05:41 +0800

    H2数据库持久化

--- src/main/java/com/example/order/OrderBean.java
+++ src/main/java/com/example/order/OrderBean.java
@@ -8,9 +8,15 @@ import org.springframework.statemachine.annotation.WithStateMachine;
 @WithStateMachine(id = "orderStateMachine")
 @Slf4j
 public class OrderBean {
+
+    /**
+     * 订单从提交到支付事件的监听
+     * @param message
+     */
     @OnTransition(source = "SUBMITTED", target = "PAID")
     public void toStatePaid(Message message) {
         String orderId = message.getHeaders().get("orderId").toString();
         log.info("----------toStatePaid,{}", orderId);
+        log.info("下面请开始写业务逻辑：。。。无需判断状态和事件，无需关心订单状态");
     }
 }
